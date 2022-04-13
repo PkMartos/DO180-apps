839fcf8e844932f7cb9cd7bd1ed93265fe9baced
Student User <student@workstation.lab.example.com>
2022-04-13 03:07:29 -0400

    Changed the index page content. v2

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Manuelita fue a la montaña a por karakoles . "\n";
+print "Manuelita fue a la montaña a por karakoles . \n";
 ?>
